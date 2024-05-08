21c20f8fba578df6cddc326203de2a112280d2c7
bluestar_ <flamekung123146@gmail.com>
2024-05-09 04:03:37 +0700

    Update main.js

--- main.js
+++ main.js
@@ -5,22 +5,21 @@ async function fetchDataAndRender() {
     const container = document.getElementById('container');
 
     for (const key in data) {
-      const { description, isUp, time, url, pingTimes } = data[key];
+      const { description, isUp, time, url, responseTime_ms } = data[key];
       const card = document.createElement('div');
       card.classList.add('card');
       const status = isUp ? 'Alive' : 'Dead';
       const statusClass = isUp ? 'alive' : 'dead';
-      const date = new Date(time);
+      const date = new Date(time * 1000); // Convert Unix timestamp to milliseconds
       const formattedDate = `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')} ${String(date.getHours()).padStart(2, '0')}:${String(date.getMinutes()).padStart(2, '0')}:${String(date.getSeconds()).padStart(2, '0')}`;
 
-
       card.innerHTML = `
-                <div class="description">${description}</div>
-                <div>Service: <span class="service-url">${url}</span></div>
-                <div>Status: <span class="${statusClass}">${status}</span></div>
-                <div>Latency: ${pingTimes[0].responseTime !== null ? pingTimes[0].responseTime + 'ms' : 'N/A'}</div>
-                <div>Last checked: ${formattedDate}</div>
-            `;
+        <div class="description">${description}</div>
+        <div>Service: <span class="service-url">${url}</span></div>
+        <div>Status: <span class="${statusClass}">${status}</span></div>
+        <div>Latency: ${responseTime_ms !== null ? responseTime_ms + 'ms' : 'N/A'}</div>
+        <div>Last checked: ${formattedDate}</div>
+      `;
       container.appendChild(card);
     }
   } catch (error) {
